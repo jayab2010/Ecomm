3205e7086d34a4d7c30cbced49315ab5cc849e66
jayab2010 <jayab2010@gmail.com>
2022-08-03 02:23:27 -0400

    added images

---
+++ frontend/ecomm/src/data.js
@@ -0,0 +1,52 @@
+const data = {
+    products:[
+        {
+            name:'Nike shirt',
+            slug:'nike shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg', //679*829px
+            price:120,
+            counterInStock:10,
+            brand:'Nike',
+            rating:4,
+            numReviews:12,
+            description:'high quality product'
+        },
+        {
+            name:'Nike pants',
+            slug:'nike pants',
+            category: 'Pants',
+            image:'/images/p2.jpg',
+            price:120,
+            counterInStock:10,
+            brand:'Nike',
+            rating:4,
+            numReviews:12,
+            description:'high quality product'
+        },
+        {
+            name:'Nike Jogger',
+            slug:'nike shirt',
+            category: 'shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            counterInStock:10,
+            brand:'Nike',
+            rating:4,
+            numReviews:12,
+            description:'high quality product'
+        },
+        {
+            name:'MK shirt',
+            slug:'MK shirt',
+            category: 'shirts',
+            image:'/images/p4.jpg',
+            price:120,
+            counterInStock:10,
+            brand:'Nike',
+            rating:4,
+            numReviews:12,
+            description:'high quality product'
+        },
+    ],
+};
