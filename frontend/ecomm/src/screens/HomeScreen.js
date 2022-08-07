2b2e6a9c60c9a5e2b539f6cc4e96b5323d5bdded
jayab2010 <jayab2010@gmail.com>
2022-08-07 01:47:51 -0400

    added useReducer

--- frontend/ecomm/src/screens/HomeScreen.js
+++ frontend/ecomm/src/screens/HomeScreen.js
@@ -1,20 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import logger from "use-reducer-logger";
 //import data from "../data";
 
+const reducer = (state, action)=> {
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return {...state, loading: true};
+      case 'FETCH_SUCESS':
+        return {...state, products:action.payload, loading: true};
+        case 'FETCH_FAIL':
+          return {...state, loading: false, error:action.payload};
+          default:
+            return state;
+          }
+        };
+
+
+
+
 function HomeScreen(){
-  const [products, setProducts]= useState([]);
+const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+  products:[],
+  loading: true,
+  error: '',
+});
+  //const [products, setProducts]= useState([]);
   useEffect(() =>{
     const fetchData = async () =>{
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch ({type: 'FETCH_REQUEST' });
+      try{
+        const result = await axios.get('/api/products');
+        dispatch ({type: 'FETCH_SUCESS', payload: result.data});
+      }catch(err){
+       dispatch({type: 'FETCH_FAIL', payload: err.message }); 
+      }
+      
+      //setProducts(result.data);
     };
     fetchData();
   },[]);
-return <div><h1>Featured Product</h1>
+return(
+<div>
+  <h1>Featured Product</h1>
     <div className="products">
-    {products.map((product) => (
+    {
+    products.map((product) => (
         <div className="product" key={product.slug}>
           <Link to={`/product/${product.slug}`}>
           <img src={product.image} alt={product.name}/>
@@ -27,7 +59,10 @@ return <div><h1>Featured Product</h1>
           <button>Add to cart</button>
           </div>
           </div>
-      ))}
-      </div></div>
-}
+      ))
+    }
+      </div>
+      </div>
+);
+    }
 export default HomeScreen;
