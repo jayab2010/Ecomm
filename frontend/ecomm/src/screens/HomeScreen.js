dca6852a5ed8437bc4626687fce645a31b272dbe
jayab2010 <jayab2010@gmail.com>
2022-08-12 02:36:53 -0400

    updated productscreen page

--- frontend/ecomm/src/screens/HomeScreen.js
+++ frontend/ecomm/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import logger from "use-reducer-logger";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
 //import data from "../data";
 
 const reducer = (state, action)=> {
@@ -46,8 +47,12 @@ const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
   },[]);
 return(
 <div>
+  <Helmet>
+    <title>ECOMM</title>
+  </Helmet>
   <h1>Featured Product</h1>
     <div className="products">
+      
       <Row>
     {
     products.map((product) => (
